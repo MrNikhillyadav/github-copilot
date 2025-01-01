792226f375467f2110bef66cb5a195eda5e23d14
Nikhil Yadav <facts.foundr@gmail.com>
2025-01-01 23:22:57 +0530

    modified ui

--- app/sections/Assistance.tsx
+++ app/sections/Assistance.tsx
@@ -10,16 +10,16 @@ import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
 
 export function Assistance(){
     return(
-        <div className ="bg-[rgba(21,26,33,255)] px-[8rem] pb-20  ">
+        <div className ="bg-[rgba(21,26,33,255)] md:px-[8rem] md:pb-20  ">
             <div
              style={{
                 backgroundImage :`url(${gradient.src})`,
                 backgroundPosition: 0 ,
                 height: 700,
-            }} className=" inline-flex flex-col justify-center items-center gap-8  w-full rounded-3xl ">
+            }} className=" bg-yellow-300 mx-8 md:mx-0 px-8 md:px-0 object-cover inline-flex overflow-x-clip flex-col justify-center items-center md:gap-8  md:w-full rounded-3xl ">
 
-                <h1 className="text-[3.2rem] tracking-tight font-semibold ">Ask for assistance <br /> right in your terminal</h1>
-                <p className="text-2xl inline-flex gap-4 items-center justify-center  font-semibold">Try copilot in the CLI  <IoIosArrowForward/></p>
+                <h1 className="md:text-[3.2rem] tracking-tight font-semibold ">Ask for assistance <br /> right in your terminal</h1>
+                <p className=" md:text-2xl inline-flex gap-4 items-center justify-center  font-semibold">Try copilot in the CLI  <IoIosArrowForward/></p>
                 <Image 
                         src={explain}
                         width={900}
@@ -28,8 +28,8 @@ export function Assistance(){
                         />    
             </div>
 
-            <div className='flex  py-[2rem] gap-12 text-center items-center justify-between'>
-                <div className=" bg-white/5   rounded-2xl gap-8 items-start justify-center drop-shadow-md p-16 py-20 flex flex-col ">
+            <div className='flex  flex-col md:flex-row py-[2rem] gap-12 text-center items-center justify-between'>
+                <div className=" bg-white/5   rounded-2xl gap-8 items-start justify-center drop-shadow-md md:p-16 md:py-20 flex flex-col ">
                         <div className="w-20 h-20 inline-flex items-center justify-center text-3xl rounded-full bg-blue-600">
                             <HiOutlineDevicePhoneMobile/>
                         </div>
