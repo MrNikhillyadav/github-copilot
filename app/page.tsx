b28c63b1d79fe6574e4628230613bfd8553972f1
Nikhil Yadav <facts.foundr@gmail.com>
2025-01-02 23:30:30 +0530

    add FaQs.tsx component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import {Hero} from "./sections/Hero";
 import {Features} from "./sections/Features"
 import {Assistance} from "./sections/Assistance"
 import {Footer} from "./sections/Footer"
+import FaQs from "./sections/FaQs";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero/>
       <Features/>
       <Assistance/>
+      <FaQs/>
       <Footer />
     </div>
   );
---
+++ app/sections/FaQs.tsx
@@ -0,0 +1,9 @@
+export default function FaQs(){
+    return (
+        <div>
+            <div>
+                Frequently asked questions
+            </div>
+        </div>
+    )
+}
